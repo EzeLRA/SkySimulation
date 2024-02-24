bf5d94e9baaa22228806e304bbc29f5f47934f75
EzeLRA <ezeleocraft@gmail.com>
2024-02-24 20:31:59 -0300

    cambios

--- classes.py
+++ classes.py
@@ -1,14 +1,24 @@
 from config import *
 import pygame as pg
 import sys
+import ephem
+import datetime
 
-# Principal Object
+#
+# Objects (Astros) class
+#
+
 class Object:
 	def __init__(self,ScreenDisplay,coords,typeObject):
+		# Classifies Objects
 		self.typeClass = typeObject
+		# Actual Using Window
 		self.screen = ScreenDisplay
+		# Coords for the actual object
 		self.posX = coords[0]
 		self.posY = coords[1]
+
+		# Sets Color type for the actual object
 		if self.typeClass == PLANET:
 			self.scale = 5
 			self.color = "BROWN"
@@ -25,7 +35,76 @@ class Object:
 
 
 
-# Circle Border
+
+#
+# Principal Observer Class:
+#
+
+class Observatory:
+	def __init__(self):
+		self.actualObserver = ephem.Observer()
+
+	#
+	# Computing Funtions
+	#
+	
+	def computeMoonCoords(self):
+		# Init moon coords with the actual parameters seted in the actual observatory
+		self.astro = ephem.Moon()
+		self.astro.compute(self.actualObserver)
+
+	#
+	# Seters Functions
+	#
+
+	def setGeoLatLong(self,lat,lon):
+		# Set determinated location for the observatory
+		self.actualObserver.lat = lat
+		self.actualObserver.lon = lon
+
+	def setGeoDate(self,ingresedDate):
+		# Set a determinated Date Time for the observatory
+		self.actualObserver.date = ephem.Date(ingresedDate)
+
+	#
+	# Geters Functions
+	#
+
+	def getActualAzimutDegrees(self):
+		azimut = self.astro.az
+		azimut_degrees = ephem.degrees(azimut) # Radians to Degrees
+		return azimut_degrees
+
+	def getActualAltitudDegrees(self):
+		altitud = self.astro.alt
+		altitud_degrees = ephem.degrees(altitud) # Radians to Degrees
+		return altitud_degrees
+
+	def getActualAzimutRadians(self):
+		azimut = self.astro.az
+		return azimut.real
+
+	def getActualAltitudRadians(self):
+		altitud = self.astro.alt
+		return altitud.real
+
+	def getActualDate(self):
+		# Actual Local Date
+		actualDate = datetime.datetime.now()
+		return actualDate
+
+	def getActualUTCDate(self):
+		# Actual UTC Date format
+		actualUTCDate = datetime.datetime.utcnow()
+		return actualUTCDate
+
+
+
+
+#
+# Circle Border Class
+#
+
 class CircleBorder:
 	def __init__(self,ScreenDisplay):
 		self.centerCoords = [(WIN_SIZE_PREDEFINED[0] / 2)-150,WIN_SIZE_PREDEFINED[1] / 2]
@@ -54,7 +133,11 @@ class CircleBorder:
 		pg.draw.circle(self.screen,"WHITE",self.centerCoords,self.radius,self.borderSize)
 
 
-# Principal Structure
+
+#
+# Escenary Estructure Class (Init)
+#
+
 class EscenaryInit:
 	def __init__(self,win_size):
 		# init pygame modules
@@ -67,6 +150,16 @@ class EscenaryInit:
 		self.object1 = Object(self.screen,self.border.centerCoords,PLANET)
 		# clock
 		self.clock = pg.time.Clock()
+
+		Obs1 = Observatory()
+		Obs1.setGeoLatLong(lat='-34.36',lon='-58.26')
+		Obs1.setGeoDate(Obs1.getActualUTCDate())
+		Obs1.computeMoonCoords()
+		print(Obs1.getActualDate())
+		print(Obs1.getActualAltitudDegrees())
+		print(Obs1.getActualAzimutDegrees())
+		print(Obs1.getActualAltitudRadians())
+		print(Obs1.getActualAzimutRadians())
 		
 
 	def check_events(self):
